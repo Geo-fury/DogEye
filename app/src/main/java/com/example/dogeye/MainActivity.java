e2996b144cb6e3c708322c7cab296b783ec6f962
Giovanni <geoegbuonu@gmail.com>
2024-07-10 12:50:36 +0400

    aesthetic changes

--- app/src/main/java/com/example/dogeye/MainActivity.java
+++ app/src/main/java/com/example/dogeye/MainActivity.java
@@ -24,7 +24,6 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-
 import org.tensorflow.lite.DataType;
 import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
 
@@ -36,115 +35,111 @@ import com.example.dogeye.ml.Model;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button camera, gallery;
-    ImageView imageView;
-    TextView result;
-    int imageSize = 224;
+    private Button cameraButton, galleryButton;
+    private ImageView imageView;
+    private TextView resultTextView;
+    private static final int IMAGE_SIZE = 224;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        camera = findViewById(R.id.button);
-        gallery = findViewById(R.id.button2);
+        initializeViews();
 
-        result = findViewById(R.id.result);
-        imageView = findViewById(R.id.imageView);
-
-        camera.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
-                    Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                    startActivityForResult(cameraIntent, 3);
-                } else {
-                    requestPermissions(new String[]{Manifest.permission.CAMERA}, 100);
-                }
+        cameraButton.setOnClickListener(view -> {
+            if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, 3);
+            } else {
+                requestPermissions(new String[]{Manifest.permission.CAMERA}, 100);
             }
         });
-        gallery.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent cameraIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(cameraIntent, 1);
-            }
+
+        galleryButton.setOnClickListener(view -> {
+            Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(galleryIntent, 1);
         });
     }
 
-    public void classifyImage(Bitmap image){
+    private void initializeViews() {
+        cameraButton = findViewById(R.id.camera_button);
+        galleryButton = findViewById(R.id.gallery_button);
+        imageView = findViewById(R.id.imageView);
+        resultTextView = findViewById(R.id.result);
+    }
+
+    private void classifyImage(Bitmap image) {
         try {
             Model model = Model.newInstance(getApplicationContext());
 
-            // Creates inputs for reference.
-            TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, imageSize, imageSize, 3}, DataType.FLOAT32);
-            ByteBuffer byteBuffer = ByteBuffer.allocateDirect(4 * imageSize * imageSize * 3);
+            TensorBuffer inputFeature = TensorBuffer.createFixedSize(new int[]{1, IMAGE_SIZE, IMAGE_SIZE, 3}, DataType.FLOAT32);
+            ByteBuffer byteBuffer = ByteBuffer.allocateDirect(4 * IMAGE_SIZE * IMAGE_SIZE * 3);
             byteBuffer.order(ByteOrder.nativeOrder());
 
-            int[] intValues = new int[imageSize * imageSize];
+            int[] intValues = new int[IMAGE_SIZE * IMAGE_SIZE];
             image.getPixels(intValues, 0, image.getWidth(), 0, 0, image.getWidth(), image.getHeight());
-            int pixel = 0;
-            //iterate over each pixel and extract R, G, and B values. Add those values individually to the byte buffer.
-            for(int i = 0; i < imageSize; i ++){
-                for(int j = 0; j < imageSize; j++){
-                    int val = intValues[pixel++]; // RGB
-                    byteBuffer.putFloat(((val >> 16) & 0xFF) * (1.f / 255));
-                    byteBuffer.putFloat(((val >> 8) & 0xFF) * (1.f / 255));
-                    byteBuffer.putFloat((val & 0xFF) * (1.f / 255));
-                }
+
+            for (int pixelValue : intValues) {
+                byteBuffer.putFloat(((pixelValue >> 16) & 0xFF) * (1.f / 255));
+                byteBuffer.putFloat(((pixelValue >> 8) & 0xFF) * (1.f / 255));
+                byteBuffer.putFloat((pixelValue & 0xFF) * (1.f / 255));
             }
 
-            inputFeature0.loadBuffer(byteBuffer);
+            inputFeature.loadBuffer(byteBuffer);
 
-            // Runs model inference and gets result.
-            Model.Outputs outputs = model.process(inputFeature0);
-            TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
+            Model.Outputs outputs = model.process(inputFeature);
+            TensorBuffer outputFeature = outputs.getOutputFeature0AsTensorBuffer();
 
-            float[] confidences = outputFeature0.getFloatArray();
-            // find the index of the class with the biggest confidence.
-            int maxPos = 0;
-            float maxConfidence = 0;
-            for (int i = 0; i < confidences.length; i++) {
-                if (confidences[i] > maxConfidence) {
-                    maxConfidence = confidences[i];
-                    maxPos = i;
-                }
-            }
-            String[] classes = {"French Bulldog", "German Shepherd", "Labrador Retriever", "Golden Retiever", "Siberian Husky", "Poodle", "Chihuahua", "Pomeranian", "Rottweiler", "Beagle"};
-            result.setText(classes[maxPos]);
+            float[] confidences = outputFeature.getFloatArray();
+            int maxPos = getMaxConfidenceIndex(confidences);
 
-            // Releases model resources if no longer used.
+            String[] classes = {"French Bulldog", "German Shepherd", "Labrador Retriever", "Golden Retriever", "Siberian Husky", "Poodle", "Chihuahua", "Pomeranian", "Rottweiler", "Beagle"};
+            resultTextView.setText(classes[maxPos]);
+
             model.close();
         } catch (IOException e) {
-            // TODO Handle the exception
+            e.printStackTrace();
         }
+    }
+
+    private int getMaxConfidenceIndex(float[] confidences) {
+        int maxPos = 0;
+        float maxConfidence = 0;
+        for (int i = 0; i < confidences.length; i++) {
+            if (confidences[i] > maxConfidence) {
+                maxConfidence = confidences[i];
+                maxPos = i;
+            }
+        }
+        return maxPos;
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if(resultCode == RESULT_OK){
-            if(requestCode == 3){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == 3) {
                 Bitmap image = (Bitmap) data.getExtras().get("data");
-                int dimension = Math.min(image.getWidth(), image.getHeight());
-                image = ThumbnailUtils.extractThumbnail(image, dimension, dimension);
-                imageView.setImageBitmap(image);
-
-                image = Bitmap.createScaledBitmap(image, imageSize, imageSize, false);
+                image = prepareImage(image);
                 classifyImage(image);
-            }else{
-                Uri dat = data.getData();
-                Bitmap image = null;
+            } else if (requestCode == 1) {
+                Uri imageUri = data.getData();
                 try {
-                    image = MediaStore.Images.Media.getBitmap(this.getContentResolver(), dat);
+                    Bitmap image = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+                    image = prepareImage(image);
+                    classifyImage(image);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-                imageView.setImageBitmap(image);
-
-                image = Bitmap.createScaledBitmap(image, imageSize, imageSize, false);
-                classifyImage(image);
             }
         }
-        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private Bitmap prepareImage(Bitmap image) {
+        int dimension = Math.min(image.getWidth(), image.getHeight());
+        image = ThumbnailUtils.extractThumbnail(image, dimension, dimension);
+        imageView.setImageBitmap(image);
+        return Bitmap.createScaledBitmap(image, IMAGE_SIZE, IMAGE_SIZE, false);
     }
 }
